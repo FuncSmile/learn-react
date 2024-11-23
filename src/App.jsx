f2460b7520bf95b083ee7f6ef0d986bfa2982680
FuncSmile <user.fadli3001@gmail.com>
2024-11-23 18:37:45 +0700

    create class component :mag:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        click me!
+      </button>
+    );
+  }
+}
+
 function App() {
   return (
-    <div className="font-bold text-5xl text-red-500">
-      <h1>Belajar React</h1>
+    <div className="w-full h-screen flex justify-center items-center">
+      <Button></Button>
     </div>
   );
 }
