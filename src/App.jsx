b48d1f3233581580dedda984353550d81758f640
FuncSmile <user.fadli3001@gmail.com>
2024-11-23 19:24:55 +0700

    props functional component :zap:

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Button extends React.Component {
   render() {
@@ -10,19 +11,27 @@ class Button extends React.Component {
   }
 }
 
-const ButtonFucn = () => {
+const ButtonFucn = (props) => {
+  const { color, colorHov } = props;
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button
+      className={`${color} ${colorHov} text-white font-bold py-2 px-4 rounded`}
+    >
       click me!
     </button>
   );
+};
+
+ButtonFucn.propTypes = {
+  color: PropTypes.string,
+  colorHov: PropTypes.string,
 };
 
 function App() {
   return (
     <div className="w-full h-screen flex justify-center items-center gap-4">
       <Button></Button>
-      <ButtonFucn></ButtonFucn>
+      <ButtonFucn color="bg-sky-500" colorHov="hover:bg-sky-700"></ButtonFucn>
     </div>
   );
 }
