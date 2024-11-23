ac9be8ab68a70e4d34489dbf74466155ca925a03
FuncSmile <user.fadli3001@gmail.com>
2024-11-23 18:43:43 +0700

    create fucntional component :fire:

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,19 @@ class Button extends React.Component {
   }
 }
 
+const ButtonFucn = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      click me!
+    </button>
+  );
+};
+
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center gap-4">
       <Button></Button>
+      <ButtonFucn></ButtonFucn>
     </div>
   );
 }
